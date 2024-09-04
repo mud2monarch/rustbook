7dd89a96136a02bd1693426b8c263cc4ae00d6e9
Gyges <shepherd2sovereign@gmail.com>
2024-09-03 22:13:01 -0400

    added looped check for valid, 3 part input.

--- calculator/src/main.rs
+++ calculator/src/main.rs
@@ -2,18 +2,23 @@ use std::io;
 
 fn main() {
     println!("Welcome to my calculator! What would you like to compute?");
-
+    
     let mut input = String::new();
-    io::stdin().read_line(&mut input).expect("Uh oh, your system is totally fked!");
 
-    let (first_operand, symbol, second_operand) = input.split_whitespace()
+    loop {
+        println!("Please input your equation.");
 
-    println!("input so far is {}", input);
-}
+        input.clear();
 
-enum MathProblem {
-    Add,
-    Subtract,
-    Multiply,
-    Divide
+        io::stdin().read_line(&mut input).expect("Uh oh, your system is totally fked!");
+
+        if input.split_whitespace().count() != 3 {
+            println!("Input must have exactly three parts: two operands and one operator, separated by whitespace. (e.g., \"3 + 5\"). Try again.");
+            continue;
+        }
+
+        break;
+    }
+
+    println!("Valid input! It was: {}", input);
 }
