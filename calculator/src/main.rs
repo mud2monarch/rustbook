6b4bee9adbd7d6d9c9d0de1b35ee42e13da5a3b0
Gyges <shepherd2sovereign@gmail.com>
2024-09-25 06:32:44 -0400

    wooowwwww input validation ftw :)

--- calculator/src/main.rs
+++ calculator/src/main.rs
@@ -6,27 +6,38 @@ fn main() {
     let mut input = String::new();
 
     loop {
+        input.clear();
         println!("Please input your equation.");
 
-        input.clear();
+        io::stdin().read_line(&mut input).expect("Uh oh, your system is totally fked!"); // this should not err lmao
 
-        io::stdin().read_line(&mut input).expect("Uh oh, your system is totally fked!");
+        let parts: Vec<&str> = input.split_whitespace().collect();
 
-        if input.split_whitespace().count() != 3 {
+        if parts.len() != 3 {
             println!("Input must have exactly three parts: two operands and one operator, separated by whitespace. (e.g., \"3 + 5\"). Try again.");
             continue;
         }
 
-        break;
+        // sorry for the non-idiomatic indenting, it helps me see the code.
+        match (
+            parts[0].parse::<i32>(), // parse parts[0] as an i32. this returns a Result<i32, Err(E)> (?)
+            parts[1].chars().next(), // take parts[1], return a character Iterator, then take the first value in the Iterator. This returns an Option
+            parts[2].parse::<i32>() // parse parts[0] as an i32. this returns a Result<i32, Err(E)> (?)
+            ) {
+                (
+                    Ok(num1),
+                    Some(op1),
+                    Ok(num2)
+                ) if "+-/*".contains(op1) => { // the syntax feels a little weird, but four conditions need to be true
+                    println!("Valid input! It was: {}", input);
+                    break;
+                }
+                _ => {
+                    println!("Something is wrong with your input! Try again.");
+                    continue;
+                }
+            }
     }
-
-    let parts: Vec<&str> = input.split_whitespace().collect();
-
-    let num1 = parts[0].parse::<i32>();
-    let op = parts[1].parse::<char>();
-    let num2 = parts[2].parse::<char>();
-
-    // now check
 
     println!("Valid input! It was: {}", input);
 }
