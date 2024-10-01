16aa430faa7d28a66269114032c6a8a2a3c3a8a5
Gyges <shepherd2sovereign@gmail.com>
2024-10-01 07:12:54 -0400

    excellent work on piglatinfy

--- piglatinfy/src/main.rs
+++ piglatinfy/src/main.rs
@@ -7,6 +7,17 @@ fn main() {
     io::stdin().read_line(&mut input).expect("reading of input errored.");
 
     let parts: Vec<&str> = input.split_whitespace().collect();
+    let mut output: Vec<&str> = Vec::new();
+
+    for word in &parts {
+        if is_alphabetic(word) {
+            match pig_latinfy(word) {
+                Some(pig_word) => output.push(pig_word),
+                None => panic!("Unexpected empty word in pig_latinfy: {}", word),
+            }
+        }
+        // @dev TODO: next up
+    }
 }
 
 fn is_alphabetic (s: &str) -> bool {
@@ -16,4 +27,27 @@ fn is_alphabetic (s: &str) -> bool {
         }
     }
     return true;
+}
+
+// note: usage should check if the full input is_ascii_alphabetic() prior to passing to this function.
+fn pig_latinfy(word: &str) -> Option<String> {
+    let mut chars: Vec<char> = word.chars().collect();
+
+    if chars.is_empty() {
+        return None;
+    }
+
+    let first_char = chars[0].to_lowercase().next().unwrap();
+    
+    // If it starts with a vowel, just add "ay"
+    if matches!(first_char, 'a' | 'e' | 'i' | 'o' | 'u') {
+        chars.extend(['a', 'y']);
+    }
+    // because I check if it's is_ascii_alphabetic() before, I can move straight to an else
+    else {
+        let first = chars.remove(0);
+        chars.extend([first, 'a', 'y']);
+    }
+
+    Some(chars.into_iter().collect())
 }
